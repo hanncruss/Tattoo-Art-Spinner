09de671b64233db1f279a618d6b7a0e8a9601272
hanncruss <hanncruss@gmail.com>
2026-06-04 11:00:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -138,10 +138,13 @@ function spin() {
   const landingIndex =
     landingLoop * artworks.length + originalIndex;
 
+  const centerAdjustment = cardWidth * 0.08;
+
   const finalX =
     windowWidth / 2 -
     landingIndex * cardStep -
-    cardWidth / 2;
+    cardWidth / 2 +
+    centerAdjustment;
 
   spinnerTrack.style.transition = "none";
   spinnerTrack.style.transform = `translateX(${startX}px)`;
